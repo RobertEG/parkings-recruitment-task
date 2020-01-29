9ce8ee0aa5543f4c5841a60a175d60ce4e3523d0
RobertEG <robpiotro@o2.pl>
2020-01-29 20:34:56 +0100

    Updates

---
+++ precache-manifest.73b663dfba4cbdad5aeba0329cfe9ebe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6980452c84fec1d3fc9a2142d3c9570",
+    "url": "/parkings-recruitment-task/index.html"
+  },
+  {
+    "revision": "c931ac697546fe55a89a",
+    "url": "/parkings-recruitment-task/static/css/main.26949fb9.chunk.css"
+  },
+  {
+    "revision": "9d089bab8669020d74a8",
+    "url": "/parkings-recruitment-task/static/js/2.3ad5daf6.chunk.js"
+  },
+  {
+    "revision": "4302cec8d33c1a3a4473a3db31f00eca",
+    "url": "/parkings-recruitment-task/static/js/2.3ad5daf6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c931ac697546fe55a89a",
+    "url": "/parkings-recruitment-task/static/js/main.03fe38b9.chunk.js"
+  },
+  {
+    "revision": "40b3f54001a11bbecc74",
+    "url": "/parkings-recruitment-task/static/js/runtime-main.5173f3c9.js"
+  }
+]);
